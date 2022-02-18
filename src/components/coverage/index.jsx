8ddd3ed17f5cbc274716d5308d261a5439767b89
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-02-18 13:54:51 +0600

    change bg for coverate and price calc section

--- src/components/coverage/index.jsx
+++ src/components/coverage/index.jsx
@@ -4,24 +4,26 @@ import AreaList from "./area-list";
 
 const Coverage = () => {
   return (
-    <Container className="py-5">
-      <Row>
-        <Col sm="12" lg="6" className="p-3">
-          <h6 className="subtitle">Coverage</h6>
-          <h2 className="h2 text-bolder mb-4">
-            We provide a <br />
-            wide <span className="text-primary">Coverage</span>
-          </h2>
-          <p style={{ maxWidth: "30rem" }}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam
-            quaerat veniam culpa quidem.
-          </p>
-          <Button variant="secondary mt-3 mb-5">Full List</Button>
-        </Col>
-        <Col className="">
-          <AreaList />
-        </Col>
-      </Row>
+    <Container fluid className="py-5 bg-gray">
+      <Container className="container-lg">
+        <Row>
+          <Col sm="12" lg="6" className="p-3">
+            <h6 className="subtitle">Coverage</h6>
+            <h2 className="h2 text-bolder mb-4">
+              We provide a <br />
+              wide <span className="text-primary">Coverage</span>
+            </h2>
+            <p style={{ maxWidth: "30rem" }}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam
+              quaerat veniam culpa quidem.
+            </p>
+            <Button variant="secondary mt-3 mb-5">Full List</Button>
+          </Col>
+          <Col className="">
+            <AreaList />
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container fluid className="bg-gray py-5">
+      <Container className="container-lg"></Container>
+    </Container>
+  );
+};
+
+export default Footer;
