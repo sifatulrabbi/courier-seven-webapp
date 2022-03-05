8002d8750e727df8024044038139b27915d22e41
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-05 15:42:09 +0600

    ref: refactor hero, navbar, about, footer

--- src/components/navbar/nav-menu.jsx
+++ src/components/navbar/nav-menu.jsx
@@ -2,20 +2,18 @@ import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { v4 } from 'uuid';
 
-const NavMenu = ({ links, showMenu, setShowMenu, transparent }) => {
+const NavMenu = ({ links, showMenu, setShowMenu }) => {
   return (
     <div
-      className={`justify-content-end ${
-        transparent ? 'transparent' : 'white'
-      } ${showMenu ? 'active' : 'hide'} navbar-nav-menu`}
+      className={`justify-content-end bg-white ${
+        showMenu ? 'active' : 'hide'
+      } navbar-nav-menu`}
     >
       <Nav className='d-flex align-items-center justify-content-center'>
         {links.map((item) => (
           <Link
             key={v4()}
-            className={`nav-link text-dark p-2 px-3 ${
-              transparent ? 'text-white' : 'text-dark'
-            } menu-item`}
+            className='nav-link p-2 px-3 text-dark'
             to={item.path}
             onClick={() => setShowMenu(false)}
           >
