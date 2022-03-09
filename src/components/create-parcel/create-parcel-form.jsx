7cf79334b333f164629223bb8c16fa67b3205be5
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-09 11:08:54 +0600

    update manifest.json

--- src/components/create-parcel/create-parcel-form.jsx
+++ src/components/create-parcel/create-parcel-form.jsx
@@ -22,98 +22,121 @@ const CreateParcelForm = () => {
         e.preventDefault();
         console.log(e);
       }}
+      className='bg-white rounded shadow-sm'
+      style={{ height: 'max-content' }}
     >
-      <Row>
-        <WrappedForm
-          type='text'
-          label='Customer Name'
-          name='customer_name'
-          placeholder='e.g. John Doe'
-          value={hook.customerName}
-          onChange={hook.handleCustomerName}
-        />
-        <WrappedForm
-          type='number'
-          label='Customer mobile'
-          name='customer_mobile'
-          placeholder='01XXXXXXXXX'
-          value={hook.invoice}
-          onChange={hook.handleInvoice}
-        />
-        <WrappedForm
-          type='number'
-          label='Collection amount'
-          name='collection_amount'
-          placeholder='e.g. 100 TK'
-          value={hook.collection}
-          onChange={hook.handleCollection}
-        />
-        <WrappedForm
-          type='number'
-          label='Product price'
-          name='product_price'
-          placeholder='e.g. 100 TK'
-          value={hook.productPrice}
-          onChange={hook.handleProductPrice}
-        />
-        <WrappedForm
-          type='number'
-          label='Product Weight (gm)'
-          name='product_weight'
-          placeholder='e.g. 100'
-          value={hook.productWeight}
-          onChange={hook.handleProductWeight}
-        />
-        <WrappedForm
-          type='select'
-          label='Product Type'
-          name='product_type'
-          placeholder='e.g. Solid'
-          value={hook.productType}
-          onChange={hook.handleProductType}
-        />
-        <WrappedForm
-          type='text'
-          label='Delivery Division'
-          name='delivery_division'
-          placeholder='Delivery Division'
-          value={hook.deliveryDivision}
-          onChange={hook.handleDeliveryDivision}
-        />
-        <WrappedForm
-          type='text'
-          label='Delivery District'
-          name='delivery_district'
-          placeholder='Delivery District'
-          value={hook.deliveryDistrict}
-          onChange={hook.handleDeliveryDistrict}
-        />
-        <WrappedForm
-          type='text'
-          label='Delivery Upazila'
-          name='delivery_upazila'
-          placeholder='Delivery Upazila'
-          value={hook.deliveryUpazila}
-          onChange={hook.handleDeliveryUpazila}
-        />
-        <WrappedForm
-          type='text'
-          label='Delivery Street'
-          name='delivery_street'
-          placeholder='Delivery Street'
-          value={hook.deliveryArea}
-          onChange={hook.handleDeliveryArea}
-        />
-        <WrappedForm
-          type='text'
-          label='Delivery House'
-          name='delivery_house'
-          placeholder='Delivery House'
-          value={hook.deliveryStreet}
-          onChange={hook.handleDeliveryStreet}
-        />
+      <Row className='g-4 p-3'>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='text'
+            label='Customer Name'
+            name='customer_name'
+            placeholder='e.g. John Doe'
+            value={hook.customerName}
+            onChange={hook.handleCustomerName}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='number'
+            label='Customer mobile'
+            name='customer_mobile'
+            placeholder='01XXXXXXXXX'
+            value={hook.invoice}
+            onChange={hook.handleInvoice}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='number'
+            label='Collection amount'
+            name='collection_amount'
+            placeholder='e.g. 100 TK'
+            value={hook.collection}
+            onChange={hook.handleCollection}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='number'
+            label='Product price'
+            name='product_price'
+            placeholder='e.g. 100 TK'
+            value={hook.productPrice}
+            onChange={hook.handleProductPrice}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='number'
+            label='Product Weight (gm)'
+            name='product_weight'
+            placeholder='e.g. 100'
+            value={hook.productWeight}
+            onChange={hook.handleProductWeight}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='select'
+            label='Product Type'
+            name='product_type'
+            placeholder='e.g. Solid'
+            value={hook.productType}
+            onChange={hook.handleProductType}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='text'
+            label='Delivery Division'
+            name='delivery_division'
+            placeholder='Delivery Division'
+            value={hook.deliveryDivision}
+            onChange={hook.handleDeliveryDivision}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='text'
+            label='Delivery District'
+            name='delivery_district'
+            placeholder='Delivery District'
+            value={hook.deliveryDistrict}
+            onChange={hook.handleDeliveryDistrict}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='text'
+            label='Delivery Upazila'
+            name='delivery_upazila'
+            placeholder='Delivery Upazila'
+            value={hook.deliveryUpazila}
+            onChange={hook.handleDeliveryUpazila}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='text'
+            label='Delivery Street'
+            name='delivery_street'
+            placeholder='Delivery Street'
+            value={hook.deliveryArea}
+            onChange={hook.handleDeliveryArea}
+          />
+        </Col>
+        <Col sm='12' lg='6'>
+          <FormGroup
+            type='text'
+            label='Delivery House'
+            name='delivery_house'
+            placeholder='Delivery House'
+            value={hook.deliveryStreet}
+            onChange={hook.handleDeliveryStreet}
+          />
+        </Col>
       </Row>
-      <Button type='submit'>Create</Button>
       <div className='p-3 bg-light d-flex flex-column'>
         <p>
           Sending illegal products is completely prohibited. To learn more read
@@ -130,6 +153,10 @@ const CreateParcelForm = () => {
           value={hook.acceptTerms}
           onChange={hook.handleAcceptTerms}
         />
+      </div>
+      <div className='p-3 d-flex justify-content-between w-100'>
+        <Button variant='secondary'>Cancel</Button>
+        <Button>Create</Button>
       </div>
     </Form>
   );
--- src/hooks/use-create-parcel.js
+++ src/hooks/use-create-parcel.js
@@ -17,6 +17,72 @@ export function useCreateParcel() {
   const [acceptTerms, setAcceptTerms] = React.useState(false);
   const [shopId, setShopId] = React.useState('');
 
+  const handlers = {};
+
+  handlers.handleInvoice = function (e) {
+    setInvoice(e.target.value);
+  };
+
+  handlers.handleCollection = function (e) {
+    setCollection(e.target.value);
+  };
+
+  handlers.handleCustomerName = function (e) {
+    setCustomerName(e.target.value);
+  };
+
+  handlers.handleCustomerMobile = function (e) {
+    setCustomerMobile(e.target.value);
+  };
+
+  handlers.handleDeliveryDivision = function (e) {
+    setDeliveryDivision(e.target.value);
+  };
+
+  handlers.handleDeliveryDistrict = function (e) {
+    setDeliveryDistrict(e.target.value);
+  };
+
+  handlers.handleDeliveryUpazila = function (e) {
+    setDeliveryUpazila(e.target.value);
+  };
+
+  handlers.handleDeliveryArea = function (e) {
+    setDeliveryArea(e.target.value);
+  };
+
+  handlers.handleDeliveryStreet = function (e) {
+    setDeliveryStreet(e.target.value);
+  };
+
+  handlers.handleDeliveryHouse = function (e) {
+    setDeliveryHouse(e.target.value);
+  };
+
+  handlers.handleProductPrice = function (e) {
+    setProductPrice(e.target.value);
+  };
+
+  handlers.handleProductWeight = function (e) {
+    setProductWeight(e.target.value);
+  };
+
+  handlers.handleProductType = function (e) {
+    setProductType(e.target.value);
+  };
+
+  handlers.handleAcceptTerms = function (e) {
+    setAcceptTerms(e.target.value);
+  };
+
+  handlers.updateShopId = function (e) {
+    setShopId(e.target.value);
+  };
+
+  handlers.handleSubmit = function (e) {
+    e.preventDefault();
+  };
+
   return {
     invoice,
     collection,
@@ -33,64 +99,6 @@ export function useCreateParcel() {
     productType,
     acceptTerms,
     shopId,
-    handleInvoice: function (e) {
-      setInvoice(e.target.value);
-    },
-
-    handleCollection: function (e) {
-      setCollection(e.target.value);
-    },
-
-    handleCustomerName: function (e) {
-      setCustomerName(e.target.value);
-    },
-
-    handleCustomerMobile: function (e) {
-      setCustomerMobile(e.target.value);
-    },
-
-    handleDeliveryDivision: function (e) {
-      setDeliveryDivision(e.target.value);
-    },
-
-    handleDeliveryDistrict: function (e) {
-      setDeliveryDistrict(e.target.value);
-    },
-
-    handleDeliveryUpazila: function (e) {
-      setDeliveryUpazila(e.target.value);
-    },
-
-    handleDeliveryArea: function (e) {
-      setDeliveryArea(e.target.value);
-    },
-
-    handleDeliveryStreet: function (e) {
-      setDeliveryStreet(e.target.value);
-    },
-
-    handleDeliveryHouse: function (e) {
-      setDeliveryHouse(e.target.value);
-    },
-
-    handleProductPrice: function (e) {
-      setProductPrice(e.target.value);
-    },
-
-    handleProductWeight: function (e) {
-      setProductWeight(e.target.value);
-    },
-
-    handleProductType: function (e) {
-      setProductType(e.target.value);
-    },
-
-    handleAcceptTerms: function (e) {
-      setAcceptTerms(e.target.value);
-    },
-
-    updateShopId: function (e) {
-      setShopId(e.target.value);
-    },
+    ...handlers,
   };
 }
